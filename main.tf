1bdcb16c13a33a4095dc906c07269f36755e7a74
dhirendra4002 <86907513+dhirendra4002@users.noreply.github.com>
2022-09-23 15:59:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIARZP47FIYQH6DF5HX"
+  secret_key = "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"
   region     = "ap-south-1"
 }
 
 variable "privatekey" {
-  default = "/etc/ansible/xyz.pem"
+  default = "/etc/ansible/dp2terraform.pem"
 }
 resource "aws_security_group" "splunk" {
   name        = "terraform-splunk"
@@ -39,27 +39,21 @@ resource "aws_instance" "web" {
     host        = "${self.public_ip}"
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("xyz.pem")
+    private_key = file("dp2terraform.pem")
   }
 
   tags = {
     Name = "Prod"
   }
-  provisioner "remote-exec" {
-    inline = [
-      "ping -c 10 8.8.8.8",
-    ]
-  }
+ #provisioner "remote-exec" {
+  #  inline = [
+   #   "ping -c 10 8.8.8.8",]
+  #}
 
-  provisioner "local-exec" {
-    command = "ansible-playbook -u ubuntu -i ${aws_instance.web.public_ip}, --private-key ${var.privatekey} play.yml"
-  }
+ # provisioner "local-exec" {
+  #  command = "ansible-playbook -u ubuntu -i ${aws_instance.web.public_ip}, --private-key ${var.privatekey} play.yml"
+  #}
 }
 output "web_ip" {
   value = aws_instance.web.public_ip
 }
-##################ansible.cfg############################
-
-[defaults]
-remote_user=ubuntu
-host_key_checking= False
